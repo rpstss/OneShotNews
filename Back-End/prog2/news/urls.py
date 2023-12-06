7bb84604f6ca8260ee3b8c411317d4f6a5a7b3aa
Park Seo Hyun <127698830+rpstss@users.noreply.github.com>
2023-12-06 13:24:16 +0900

    Delete Back-End directory

--- Back-End/prog2/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("kr", views.KorNewsview.as_view()),
-    path("image", views.Imageview.as_view()),
-    path("en", views.EngNewsview.as_view()),
-    path("make-error", views.makeError),
-]
--- Back-End/prog2/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    avatar = models.TextField(null=True, blank=True)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False,)
-
